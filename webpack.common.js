00872fd3bb88ee6b5bedd572f8046c82b6ce62ff
JayCodeGitHub <JayCode.contakt@gmail.com>
2022-12-16 20:03:07 +0100

    optimized webpack build css

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,5 @@
 const path = require("path");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 
 const pages = ["index", "about"];
@@ -14,19 +13,10 @@ module.exports = {
               template: `./src/pages/${page}.html`,
               filename: `${page}.html`,
             })
-        ),
-        new CleanWebpackPlugin()
+        )
       ),
     module: {
         rules: [
-            {
-                test: /\.scss$/,
-                use: [
-                    "style-loader",
-                    "css-loader",
-                    "sass-loader"
-                ]
-            },
             {
                 test: /\.html$/,
                 use: ["html-loader"]
